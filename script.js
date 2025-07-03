cdf6f1df910407ab3010888e9f5869a277051e3c
yurika-24hour <fiore.della.canzone.di.amore@gmail.com>
2025-07-03 21:53:40 +0000

    script.js

--- script.js
+++ script.js
@@ -5,12 +5,22 @@
  * 
  * 関数over100を定義しましょう。
  * over100は、100以上ならtrue、100未満だったらfalseを返します。
- * 
+ * //over100は100以上の数値だったら真値を返す
+ * //over100は100未満だったら偽値を返す
+ * 関数名：over100
+ * 引数：数値型
+ * 返り値：ブーリアン型
  * @param {number}
  * @returns {boolean}
  */
 
-// ここに関数を定義しましょう
+function over100(number){
+    if (number <= 100){
+    return true;
+} else {
+    return false;
+}
+}
 
 
 // ここからはテストなので変更を加えないでください
